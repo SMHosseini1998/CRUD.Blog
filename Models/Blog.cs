f123185e094b9afeb5a478cfa5daac92c8c30b2c
Seyed Mehdi Hosseini <61976527+SMHosseini1998@users.noreply.github.com>
2022-07-12 13:38:34 +0430

    add Column

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,16 +1,24 @@
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 namespace CRUD.Blog.Models
 {
     [Table("Blog")]
     public class Blog
     {
-
+        [Column("Id")]
+        [Display(Name="ایدی خبر")]
         public int Id { get; set; }
 
+        [Column("Title")]
+        [Display(Name="موضوع خبر")]
         public string? Title { get; set; }
 
+        [Column("ShoetDes")]
+        [Display(Name="توضیح کوتاه خبر")]
         public string? ShoetDes { get; set; }
 
+        [Column("Des")]
+        [Display(Name="توضیح  خبر")]
         public string? Des { get; set; }
 
 
